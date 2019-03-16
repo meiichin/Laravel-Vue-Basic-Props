1bdf34a2ecf42263589b557ce372b1255330b618
meiichin <meiichan3017@gmail.com>
2019-03-16 22:09:56 +0700

    [Added] auth in laravel

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/users', 'HomeController@users')->name('users');
