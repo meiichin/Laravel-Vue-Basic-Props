043be9aee86196aed8c47b3c48d6c14d59284417
meiichin <meiichan3017@gmail.com>
2019-03-16 22:20:11 +0700

    [Added] Users.vue

---
+++ resources/js/components/Users.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Users Component</div>
+
+                    <div class="card-body">
+                        <p v-for="user in users">{{user.name}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                users:[]
+            }
+        },
+        props:[
+            'data'
+        ],
+        mounted(){
+            this.users = this.data
+            console.log(this.users)
+            console.log('lala')
+            console.log(this.data)
+        }
+    }
+</script>
